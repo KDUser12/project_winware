15459cd10539a1fb556a26a223dc304bd36555ff
KDUser <128939316+KDUser12@users.noreply.github.com>
2023-07-10 09:24:53 +0200

    10/07/2023
    Creating the window #9

--- src/windows/packages/window.py
+++ src/windows/packages/window.py
@@ -1,21 +1,5 @@
 import customtkinter
 
-class Window:
-    def window_configuration(self, window):
-        pass
-
-    def window_interface(self, window):
-        pass
-
-    def window_home_interface(self, window):
-        pass
-
-
 if __name__ == '__main__':
-    window = customtkinter.CTk()
-    instance = Window()
-
-    instance.window_configuration(window)
-    instance.window_interface(window)
-    instance.window_home_interface(window)
-    window.mainloop()
+    app = customtkinter.CTk()
+    app.mainloop()
