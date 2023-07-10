4d27accee1b545284fcaec0ed37e09515c2c5348
KDUser <128939316+KDUser12@users.noreply.github.com>
2023-07-10 09:32:22 +0200

    10/07/2023
    Window configuration #10

--- src/windows/packages/window.py
+++ src/windows/packages/window.py
@@ -1,5 +1,21 @@
 import customtkinter
 
+class Window():
+    def configuration(self, window):
+        customtkinter.set_appearance_mode('dark')
+        customtkinter.set_default_color_theme('dark-blue')
+
+        window.title('WinWARE - 1.0.0')
+        window.geometry('900x500')
+
+        window.minsize(width=900, height=500)
+        window.maxsize(width=900, height=500)
+
+        window.config(background='#1B1B1B')
+
 if __name__ == '__main__':
     app = customtkinter.CTk()
+    instance = Window()
+
+    instance.configuration(app)
     app.mainloop()
